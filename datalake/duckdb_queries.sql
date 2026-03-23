75e03ef415a490d02d8d9f0bef7e20d3f1f6b163
vedantlanke <140548961+vedantlanke@users.noreply.github.com>
2026-03-23 22:26:56 +0530

    Update duckdb_queries.sql

--- datalake/duckdb_queries.sql
+++ datalake/duckdb_queries.sql
@@ -1,1 +1,52 @@
+-- Q1: List all customers with total number of orders
 
+SELECT
+    c.customer_id,
+    c.name,
+    COUNT(o.order_id) AS total_orders
+FROM read_csv_auto('datasets/customers.csv') c
+LEFT JOIN read_json_auto('datasets/orders.json') o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name
+ORDER BY total_orders DESC;
+
+
+
+-- Q2: Top 3 customers by total order value
+
+SELECT
+    c.name,
+    SUM(o.total_amount) AS total_spent
+FROM read_csv_auto('datasets/customers.csv') c
+JOIN read_json_auto('datasets/orders.json') o
+ON c.customer_id = o.customer_id
+GROUP BY c.name
+ORDER BY total_spent DESC
+LIMIT 3;
+
+
+
+-- Q3: Customers from Bangalore and their order IDs
+
+SELECT
+    c.name,
+    o.order_id,
+    o.total_amount
+FROM read_csv_auto('datasets/customers.csv') c
+JOIN read_json_auto('datasets/orders.json') o
+ON c.customer_id = o.customer_id
+WHERE c.city = 'Bangalore';
+
+
+
+-- Q4: Join view — customer name, order date, total amount, items count
+
+SELECT
+    c.name,
+    o.order_date,
+    o.total_amount,
+    o.num_items
+FROM read_csv_auto('datasets/customers.csv') c
+JOIN read_json_auto('datasets/orders.json') o
+ON c.customer_id = o.customer_id
+ORDER BY o.order_date;
